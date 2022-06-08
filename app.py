9d2ed10fcadef6f11a3a651c48e208cef2460170
DayanaReyes <78558202+DayanaReyes@users.noreply.github.com>
2022-06-08 11:30:35 -0600

    202002364

--- app.py
+++ app.py
@@ -19,6 +19,13 @@ class Lista():
             self.ultimo.anterior=auxiliar
         self.primero.anterior=self.ultimo
         self.ultimo.siguiente=self.primero
+    def buscar(self,numero):
+        aux=self.primero
+        while aux!=None:
+            if aux.dato==numero:
+                print("anterior:",aux.anterior.dato,", actual:",aux.dato,", siguiente:",aux.siguiente.dato)
+                break
+            aux=aux.siguiente
     def imprimir(self):
         aux=self.primero
         while aux!=None:
@@ -26,3 +33,14 @@ class Lista():
             aux=aux.siguiente
             if aux==self.primero:
                 break
+listanumeros=Lista()
+listanumeros.AgregarFinal(7)
+listanumeros.AgregarFinal(96)
+listanumeros.AgregarFinal(10)
+listanumeros.AgregarFinal(25)
+listanumeros.AgregarFinal(18)
+listanumeros.AgregarFinal(2)
+print("Lista de numeros: ")
+listanumeros.imprimir()
+numero=int(input("Ingrese numero: "))
+listanumeros.buscar(numero)
